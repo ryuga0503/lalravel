4c99646c27182abedc11aafaa6056d7b4d073314
ryuga0503 <20050002ir@edu.fca.ac.jp>
2022-09-23 19:53:43 +0900

    6.23

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -21,10 +21,10 @@
               @foreach ($tweets as $tweet)
               <tr class="hover:bg-grey-lighter">
                 <td class="py-4 px-6 border-b border-grey-light">
-
-                  <!-- 🔽 ここから編集 -->
                   <div class="flex">
-                    <p class="text-left text-grey-dark">{{$tweet->user->name}}</p>
+                    <a href="{{ route('follow.show', $tweet->user->id) }}">
+                      <p class="text-left text-grey-dark">{{$tweet->user->name}}</p>
+                    </a>
                     <!-- follow 状態で条件分岐 -->
                     @if(Auth::user()->followings()->where('users.id', $tweet->user->id)->exists())
                     <!-- unfollow ボタン -->
